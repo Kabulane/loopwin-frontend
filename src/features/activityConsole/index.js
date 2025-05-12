658c8ac830ca0d68b6620e4c531e1f2a38028306
Kabulane <vaitea.lamblin@gmail.com>
2025-05-12 21:38:57 +0100

    ContestDetail is now based on store state - added a carousel feature - updated ContestModel to include ContestType - Updated ContestStore to include types logic

--- src/features/activityConsole/index.js
+++ src/features/activityConsole/index.js
@@ -1,3 +1,3 @@
 import { useActivityFeed } from './store/useActivityFeed.js'
 export { default as ActivityConsole } from './ActivityConsole.vue'
-export { useActivityFeed } // ← manquant
+export { useActivityFeed }  
